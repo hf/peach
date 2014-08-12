01d16be5d2b80827e27bebba7313e28f16eb53bb
Stojan Dimitrovski <sdimitrovski@gmail.com>
2014-08-12 21:25:01 +0200

    Didn't need that.

--- npm-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export npm_config_disturl=https://gh-contractor-zcbenz.s3.amazonaws.com/atom-shell/dist
-export npm_config_target=0.11.13
-export npm_config_arch=x64
-HOME=~/.atom-shell-gyp npm install
